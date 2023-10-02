18f94cac899265005b2334ae9b4fc033ecb2e8cb
rosemelo <rosenildesmelo@gmail.com>
2023-10-02 17:53:04 -0300

    subindo as alteracões

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const numero = prompt("Diga um número:")
+const dobro = numero*2
+alert(dobro)
+
+const jantar = prompt("Valor do jantar")
+const adicional = jantar*0.1
+valorFinal = jantar+adicional
+alert(valorFinal)
+
+const total = prompt("Valor da conta?")
+const participantes = prompt("Número de participantes?")
+const valorIndividual = (total/participantes)
+alert (valorIndividual)
